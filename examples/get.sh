1c7f606cb71b3ac7c24b0b433a4bcf14c38aef75
Miguel Estrada <mestrada@us.ibm.com>
2018-10-03 14:52:14 -0400

    Use my own app, fix lint on wwapi.js

--- examples/get.sh
+++ examples/get.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dir="$(dirname "$0")"
 TOKEN=$(http -p b -f -a "$APPID:$SECRET" POST https://api.watsonwork.ibm.com/oauth/token grant_type=client_credentials  | jq -r .access_token)
-http GET https://watsonwork-webhook-proxy.mybluemix.net/webhooks/$APPID Authorization:"Bearer $TOKEN"
+http GET https://watsonwork-webhook-proxy-me.mybluemix.net/webhooks/$APPID Authorization:"Bearer $TOKEN"
--- src/wwapi.js
+++ src/wwapi.js
@@ -6,28 +6,28 @@ const log = debug('watsonwork-webhook-proxy-wwapi');
 
 export const graphql = (token, body, cb) => {
   request.post(
-        'https://api.watsonwork.ibm.com/graphql', {
-          headers: {
-            'Content-Type': 'application/graphql',
-            Authorization: 'Bearer ' + token
-          },
-          body: body
-        }, (err, res) => {
-          if (err || res.statusCode !== 200) {
-            log('Error sending message %o, %o', body, err || res.statusCode);
-            cb(err || res.statusCode);
-            return;
-          }
-          try {
-            cb(null, JSON.parse(res.body));
-            return;
-          }
-          catch (e) {
-            cb(e);
-            return;
-          }
-          // log('Send result %d, %o', res.statusCode, res.body);
-        });
+    'https://api.watsonwork.ibm.com/graphql', {
+      headers: {
+        'Content-Type': 'application/graphql',
+        Authorization: 'Bearer ' + token
+      },
+      body: body
+    }, (err, res) => {
+      if (err || res.statusCode !== 200) {
+        log('Error sending message %o, %o', body, err || res.statusCode);
+        cb(err || res.statusCode);
+        return;
+      }
+      try {
+        cb(null, JSON.parse(res.body));
+        return;
+      }
+      catch (e) {
+        cb(e);
+        return;
+      }
+      // log('Send result %d, %o', res.statusCode, res.body);
+    });
 };
 
 export const getId = (token, cb) => {
