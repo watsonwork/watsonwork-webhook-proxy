0fef1502004d4826042b61b2c8fe11393a84598e
Miguel Estrada <mestrada@us.ibm.com>
2018-10-10 20:01:08 -0400

    point to -me host

--- examples/listen.sh
+++ examples/listen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 TOKEN=$(http -p b -f -a "$APPID:$SECRET" POST https://api.watsonwork.ibm.com/oauth/token grant_type=client_credentials  | jq -r .access_token)
-node "$(dirname "$0")/../src/client.js" "$TOKEN" "wss://watsonwork-webhook-proxy.mybluemix.net/webhooks/$APPID"
+node "$(dirname "$0")/../src/client.js" "$TOKEN" "wss://watsonwork-webhook-proxy-me.mybluemix.net/webhooks/$APPID"
--- examples/register.sh
+++ examples/register.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 TOKEN=$(http -p b -f -a "$APPID:$SECRET" POST https://api.watsonwork.ibm.com/oauth/token grant_type=client_credentials  | jq -r .access_token)
-http PUT "https://watsonwork-webhook-proxy.mybluemix.net/webhooks/${APPID}" Authorization:"Bearer $TOKEN" secret="$1"
+http PUT "https://watsonwork-webhook-proxy-me.mybluemix.net/webhooks/${APPID}" Authorization:"Bearer $TOKEN" secret="$1"
